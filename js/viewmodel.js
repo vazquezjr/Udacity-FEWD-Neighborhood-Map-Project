de7548348c8fc0f8bde0a1b75edbad35502455d4
Ruben Vazquez <vazquezr@knights.ucf.edu>
2016-08-07 20:32:00 -0400

    feat: Add filterList function
    Added a function that will retrieve locations from an ajax request and place markers for those locations on the map. This function is currently not complete.

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,8 +1,14 @@
 
 var MapLocations = function(newLat, newLng) {
 
-	lat: newLat;
-	lng: newLng;
+	var lat = newLat;
+	var lng = newLng;
+	var marker = new google.maps.Marker({
+
+						position: {lat: lat, lng: lng},
+						map: map,
+
+					});
 
 };
 
@@ -10,11 +16,33 @@ function listViewModel() {
 
 	var self = this;
 
+	self.query = ko.observable('');
+	self.location = ko.observableArray([]);
+	self.foursquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=SQZ03UD0J4VY5JX4OQDTOAHKJS5L3B4IF2UMOKWCPL0XDYKR&client_secret=L3FVORF4LAN2PJOAYLKDMWOEFEXTRU3L52DYA4PCFOIWOORZ&v=20130815&near=Orlando,FL';
+
 	self.toggleMenu = function() {
 
 		$('body').toggleClass('list-hidden');
 
-	}
+	};
+
+	self.filterList = function() {
+		self.location([]);
+		self.foursquareURL = self.foursquareURL + '&query=' + self.query();
+
+		console.log(self.foursquareURL);
+
+		var results;
+
+		$.ajax(self.foursquareURL, function(data) {
+			dataType: 'json'
+		}).done(function(data) {
+			results = data;
+		});
+
+		console.log(results);
+
+	};
 
 }
 
